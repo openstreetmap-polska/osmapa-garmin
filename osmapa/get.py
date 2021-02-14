ea5b09f4f89bbec8241f8e820b5e20af4e9e9461
Andrzej Talarczyk <andrzej@talarczyk.com>
2021-02-14 18:44:01 +0100

    Corrected download link for poland-latest.osm.pbf.

--- osmapa/get.py
+++ osmapa/get.py
@@ -4,7 +4,7 @@ import os
 
 def get_osm_data_test(bin_dir, dest_dir, pbf_filename):
     print("Running get_osm_data(" + bin_dir + ", " + dest_dir + ", " + pbf_filename + ")")
-    print('{binarki}\\wget.exe -c http://download.geofabrik.de/{pbf_file} -t 5  -O {dane_osm}/{pbf_file}'.format(
+    print('{binarki}\\wget.exe -c http://download.geofabrik.de/europe/{pbf_file} -t 5  -O {dane_osm}/{pbf_file}'.format(
         binarki=bin_dir, pbf_file=pbf_filename, dane_osm=dest_dir))
 
 def get_osm_data(bin_dir, dest_dir, pbf_filename):
@@ -15,10 +15,10 @@ def get_osm_data(bin_dir, dest_dir, pbf_filename):
         pass
     ret = -1
     if platform.system() == 'Windows':
-        ret = os.system('{binarki}\\wget.exe -c http://download.geofabrik.de/{pbf_file} -t 5  -O {dane_osm}/{pbf_file}'.format(
+        ret = os.system('{binarki}\\wget.exe -c http://download.geofabrik.de/europe/{pbf_file} -t 5  -O {dane_osm}/{pbf_file}'.format(
         binarki=bin_dir, pbf_file=pbf_filename, dane_osm=dest_dir))
     elif platform.system() == 'Linux':
-        ret = os.system('wget -c http://download.geofabrik.de/{pbf_file} -t 5  -O {dane_osm}/{pbf_file}'.format(
+        ret = os.system('wget -c http://download.geofabrik.de/europe/{pbf_file} -t 5  -O {dane_osm}/{pbf_file}'.format(
         pbf_file=pbf_filename, dane_osm=dest_dir))
     else:
         raise Exception("Unsupported operating system.")
